99f5c8617db5132d85fa15fc05864f9753a8a6e5
Krithik Tamilselvan <krithikkeshant7489@gmail.com>
2025-05-27 23:58:23 -0400

    Adding the basic parsing datas such as price, added URL's for the Loblaws API

--- src/data_parsing/data_parsing/spiders/__init__.py
+++ src/data_parsing/data_parsing/spiders/__init__.py
@@ -1,4 +1,24 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
+from pathlib import Path
+
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "data"
+
+    async def start(self):
+        urls = [
+            "https://api.pcexpress.ca/pcx-bff/api/v1/products/20812144001_EA?lang=en&date=27052025&pickupType=STORE&storeId=1029&banner=loblaw",
+            "https://cdn.contentful.com/spaces/0dlg9rxz8nvy/environments/master/entries?content_type=pageReact&locale=en-CA&include=10&fields.slug=pdp"
+            #second link is not really needed right now but will be for future features
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        data = response.json()
+        yield {
+            "item": data["name"],
+            "brand name": data["brand"],
+            "price": data["price"]["reg"]
+        }#Parsing the data we need (we can just put more stuff later right now im just getting the price and brand name)
